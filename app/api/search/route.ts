454bdd8a342173a021f875922237df1c5b937547
Flavian Zuellig <flavian.zuellig@stud.unibas.com>
2025-11-17 22:14:12 +0100

    add languages to search api

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -6,9 +6,9 @@ export async function GET(request: Request) {
     const {searchParams} = new URL(request.url);
     const search = searchParams.get('search');
     if (!search || search === "" || search === " ") return new Response("No search provided", {status: 400})
-    
+
     const results = await prisma.$queryRaw`
-        SELECT title, author, summary
+        SELECT title, author, summary, "bookLanguage", "summaryLanguage"
         FROM "Book"
         WHERE title % ${search}
            OR title ILIKE '%${search}%'
