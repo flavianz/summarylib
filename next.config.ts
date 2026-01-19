4baa3a8666ba8ac0b938f7bf1642f23eb1322f83
Flavian Züllig <flavianzuellig@hotmail.com>
2026-01-19 23:07:41 +0100

    add nextjs redirecting

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,22 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     turbopack: {
         root: __dirname,
-    }
+    },
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: 'summarylib.flavianz.ch',
+                    },
+                ],
+                destination: 'https://summarylib.org/:path*',
+                permanent: true,
+            },
+        ]
+    },
 };
 
 export default nextConfig;
