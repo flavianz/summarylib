d6610f6255759a0cf129dc28dbb55a3da65a4579
Flavian Züllig <flavianzuellig@hotmail.com>
2026-01-19 22:37:39 +0100

    add middleware to get a redirect

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import {NextRequest, NextResponse} from "next/server";
+
+const CANONICAL_HOST = 'summarylib.org';
+
+export function middleware(req: NextRequest) {
+    const host = req.headers.get('host');
+
+    if (host && host !== CANONICAL_HOST) {
+        const url = req.nextUrl.clone();
+        url.host = CANONICAL_HOST;
+        url.protocol = 'https';
+
+        return NextResponse.redirect(url, 301);
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: '/:path*',
+};
